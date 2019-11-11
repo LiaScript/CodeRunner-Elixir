fe6af7b001f65d9ea3612021cab4a6a656ccf300
André Dietrich <andre.dietrich@ovgu.de>
2019-11-11 13:05:37 +0100

    updates for heroku

--- apps/lia/config/prod.exs
+++ apps/lia/config/prod.exs
@@ -11,8 +11,10 @@ use Mix.Config
 # before starting your production server.
 config :lia, LiaWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: System.get_env("HOST_URL"), port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [schema: "https", host: "radiant-crag-95782.herokuapp.com/", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +70,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod_secret.exs"
+# import_config "prod.secret.exs"
