0b21c3b60c5605f603af8ffae8eabc3157563b31
André Dietrich <andre.dietrich@ovgu.de>
2020-06-06 11:53:40 +0200

    updated secrets

--- apps/lia/config/prod.exs
+++ apps/lia/config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :lia, LiaWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [schema: "https", host: "radiant-crag-95782.herokuapp.com/", port: 443],
+  url: [schema: "https", host: System.get_env("PORT") || "localhost", port: System.get_env("PORT") || 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
